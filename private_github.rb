ab7b90af02b625d423aba49b210f6a3b6bcc26fe
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-22 23:21:14 +0000

    Message

--- private_github.rb
+++ private_github.rb
@@ -31,7 +31,7 @@ end
 
 g = GithubAPI.new
 g.user_repos("lynchd2")
-new_date = g.commits("lynchd2", "vikingcodeschool_github_api_optional")[0].split(" ")[0] = "987-07-20"
+new_date = g.commits("lynchd2", "vikingcodeschool_github_api_optional")[0].split(" ")[0] = "2016-07-22T23:21:14Z"
 
 %x`git add .`
 %x`git commit -am "Message" --date #{new_date} `
